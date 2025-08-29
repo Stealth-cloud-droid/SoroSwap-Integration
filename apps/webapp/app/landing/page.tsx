d809d62e3f6b8726fceb42f78076ba7ec747cda9
Sendi John <johnsendi727@gmail.com>
2025-08-29 12:07:17 +0100

    feat: Add FooterLanding component with branding and links for landing page

---
+++ apps/webapp/app/landing/page.tsx
@@ -0,0 +1,10 @@
+import FooterLanding from '@/components/home/footer-landing';
+
+export default function LandingPage() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      {/* Footer */}
+      <FooterLanding />
+    </div>
+  );
+}
